7353a382da03a70499ce522a96dede62c243d592
xd3v <xd3v@kali.Home>
2025-04-27 18:24:01 -0400

    Prepare for Expo Dev Client build

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,8 @@
- 
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+defaultConfig.resolver.assetExts.push('cjs');
+
+module.exports = defaultConfig; 
